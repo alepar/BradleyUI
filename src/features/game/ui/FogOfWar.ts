e25fd965cc7a358a2755a5b753715f84f6c07e50
Alexey Parfenov <aparfenov@snapchat.com>
2020-11-18 00:40:49 -0800

    Adds fog of war

---
+++ src/features/game/ui/FogOfWar.ts
@@ -0,0 +1,53 @@
+import * as PIXI from 'pixi.js'
+
+export class FogOfWar {
+
+    readonly container: PIXI.Container
+
+    private readonly visibleGraphics: PIXI.Graphics
+    private readonly observedGraphics: PIXI.Graphics
+
+    constructor() {
+        this.visibleGraphics = new PIXI.Graphics()
+        this.observedGraphics = new PIXI.Graphics()
+
+        this.container = new PIXI.Container()
+
+        const visibleLayer = new PIXI.Container()
+        visibleLayer.alpha = 0.25
+        visibleLayer.addChild(this.visibleGraphics)
+
+        const observedLayer = new PIXI.Container()
+        observedLayer.alpha = 0.5
+        visibleLayer.addChild(this.observedGraphics)
+
+        this.container.addChild(visibleLayer)
+        this.container.addChild(observedLayer)
+        this.container.filters = [new PIXI.filters.BlurFilter(15)];
+    }
+
+    // TODO https://pixijs.io/examples/#/sprite/tiling-sprite.js
+    update(mapSize: PIXI.IPointData, visible: PIXI.IPointData[], observed: PIXI.IPointData[]) {
+        const visibleGraphics = this.visibleGraphics
+        const observedGraphics = this.observedGraphics
+
+        visibleGraphics.clear()
+        observedGraphics.clear()
+
+        this.renderFog(visibleGraphics, mapSize, visible);
+        this.renderFog(observedGraphics, mapSize, observed);
+    }
+
+    private renderFog(visibleGraphics: PIXI.Graphics, mapSize: PIXI.IPointData, holePoints: PIXI.IPointData[]) {
+        visibleGraphics.beginFill(0x000000);
+        visibleGraphics.drawRect(-100000, -100000, mapSize.x * 16 + 200000, mapSize.y * 16 + 200000);
+
+        visibleGraphics.beginHole();
+        for (const p of holePoints) {
+            visibleGraphics.drawRect(p.x * 16, p.y * 16, 16, 16);
+        }
+        visibleGraphics.endHole();
+
+        visibleGraphics.endFill();
+    }
+}
--- src/features/game/ui/GameUI.ts
+++ src/features/game/ui/GameUI.ts
@@ -2,6 +2,7 @@ import {GameData} from "../state/GameData";
 import {Viewport} from "pixi-viewport";
 import * as PIXI from 'pixi.js'
 import {OverworldTerrainSpriteFactory} from "./SpriteFactories";
+import {FogOfWar} from "./FogOfWar";
 import {Character} from "./Character";
 
 export enum GamePlayMode {
@@ -17,6 +18,26 @@ export class GameControlState {
     playMode: GamePlayMode = GamePlayMode.STOP
 }
 
+function calculateVisible(heroX: number, heroY: number) {
+    const visible: PIXI.IPointData[] = [];
+    for(let x=heroX-2; x<=heroX+2; x++) {
+        for(let y=heroY-2; y<=heroY+2; y++) {
+            visible.push({x: x, y: y})
+        }
+    }
+    return visible
+}
+
+function calculateObserved() {
+    const observed: PIXI.IPointData[] = [];
+    for(let x=-2; x<=6; x++) {
+        for(let y=-2; y<=6; y++) {
+            observed.push({x: x, y: y})
+        }
+    }
+    return observed
+}
+
 export class GameUI {
     // Injected deps
     private pixiApp: PIXI.Application;
@@ -28,7 +49,7 @@ export class GameUI {
     private terrainLayer: PIXI.Container
     private gridLayer: PIXI.Container
     private characterLayer: PIXI.Container
-    // private fogLayer: PIXI.Container
+    private fogOfWar: FogOfWar
     private baseTweenTurn: number = 0
 
     // Constructed exported deps
@@ -44,8 +65,8 @@ export class GameUI {
 
         this.terrainLayer = this.buildTerrainLayer()
         this.gridLayer = this.buildGridLayer()
-        this.characterLayer = this.buildCharacterLayer()
-        // this.fogLayer = this.buildFogLayer()
+        this.characterLayer = new PIXI.Container()
+        this.fogOfWar = new FogOfWar()
 
         this.viewport.addChild(this.terrainLayer)
 
@@ -55,22 +76,28 @@ export class GameUI {
         this.characterLayer.position.set(8, 8)
         this.viewport.addChild(this.characterLayer)
 
-        // this.fogLayer.position.set(8, 8)
-        // this.viewport.addChild(this.fogLayer)
+        this.fogOfWar.container.position.set(8, 8)
+        this.viewport.addChild(this.fogOfWar.container)
 
         const hero = new Character(this.spritesheet, "character/hero");
         this.characterLayer.addChild(hero.container)
         hero.setv(1, 0)
 
+        this.fogOfWar.update(
+            this.gameData.getMap().getSize(),
+            calculateVisible(0, 0),
+            calculateObserved()
+        )
+
         // Listen for frame updates
         pixiApp.ticker.add(() => {
             hero.step()
 
-            if (hero.container.x > 4*16) {
-                hero.container.x = 4*16
+            if (hero.container.x > 4 * 16) {
+                hero.container.x = 4 * 16
                 hero.setv(0, 1)
-            } else if (hero.container.y > 4*16) {
-                hero.container.y = 4*16
+            } else if (hero.container.y > 4 * 16) {
+                hero.container.y = 4 * 16
                 hero.setv(-1, 0)
             } else if (hero.container.x < 0) {
                 hero.container.x = 0
@@ -79,6 +106,14 @@ export class GameUI {
                 hero.container.y = 0
                 hero.setv(1, 0)
             }
+
+            const heroX = Math.floor(hero.container.x/16)
+            const heroY = Math.floor(hero.container.y/16)
+            this.fogOfWar.update(
+                this.gameData.getMap().getSize(),
+                calculateVisible(heroX, heroY),
+                calculateObserved()
+            )
         });
 
     }
@@ -113,15 +148,15 @@ export class GameUI {
 
         const map = this.gameData.getMap();
         const mapSize = map.getSize();
-        for (let x=0; x < mapSize.x+1; x++) {
-            for (let y=0; y < mapSize.y+1; y++) {
+        for (let x = 0; x < mapSize.x + 1; x++) {
+            for (let y = 0; y < mapSize.y + 1; y++) {
                 const sprite = spriteFactory.create(
-                    map.getTerrainTypeAt(x-1, y-1),
-                    map.getTerrainTypeAt(x, y-1),
+                    map.getTerrainTypeAt(x - 1, y - 1),
+                    map.getTerrainTypeAt(x, y - 1),
                     map.getTerrainTypeAt(x, y),
-                    map.getTerrainTypeAt(x-1, y),
+                    map.getTerrainTypeAt(x - 1, y),
                 )
-                sprite.position.set(x*16, y*16)
+                sprite.position.set(x * 16, y * 16)
 
                 terrain.addChild(sprite)
             }
@@ -134,26 +169,21 @@ export class GameUI {
         const mapSize = map.getSize();
 
         const grid = new PIXI.Container()
-        for (let x=1; x < mapSize.x; x++) {
+        for (let x = 1; x < mapSize.x; x++) {
             const line = new PIXI.Graphics();
             line.lineStyle(1, 0xFFFFFF, 0.15);
-            line.moveTo(x*16, 0);
-            line.lineTo(x*16, 16*mapSize.y);
+            line.moveTo(x * 16, 0);
+            line.lineTo(x * 16, 16 * mapSize.y);
             grid.addChild(line);
         }
-        for (let y=1; y < mapSize.y; y++) {
+        for (let y = 1; y < mapSize.y; y++) {
             const line = new PIXI.Graphics();
             line.lineStyle(1, 0xFFFFFF, 0.15);
-            line.moveTo(0, y*16);
-            line.lineTo(16*mapSize.x, y*16);
+            line.moveTo(0, y * 16);
+            line.lineTo(16 * mapSize.x, y * 16);
             grid.addChild(line);
         }
 
         return grid
-    }
-
-    private buildCharacterLayer() {
-        const layer = new PIXI.Container()
-        return layer
     }
 }
